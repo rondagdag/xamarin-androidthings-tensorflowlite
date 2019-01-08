d8c4ede9cc75fe1aa419cba80f5bf0c9cd762b1a
rondagdag <rlyle78@gmail.com>
2019-01-08 09:39:17 -0500

    custom image classifier - using custom vision

---
+++ customimageclassifier/imageclassifier/Classifier/Recognition.cs
@@ -0,0 +1,67 @@
+﻿namespace imageclassifier
+{
+    internal class Recognition
+    {
+
+        /*
+     * A unique identifier for what has been recognized. Specific to the class, not the instance of
+     * the object.
+     */
+        string _id;
+
+        /*
+         * Display name for the recognition.
+         */
+        string _title;
+
+        /*
+         * A sortable score for how good the recognition is relative to others. Higher should be better.
+         */
+        float? _confidence;
+
+        public Recognition(
+                 string id, string title, float confidence)
+        {
+            this._id = id;
+            this._title = title;
+            this._confidence = confidence;
+        }
+
+        public string GetId()
+        {
+            return _id;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public float GetConfidence()
+        {
+            return _confidence == null ? 0f : _confidence.Value;
+        }
+
+
+        public override string ToString()
+        {
+            string resultString = "";
+            if (_id != null)
+            {
+                resultString += "[" + _id + "] ";
+            }
+
+            if (_title != null)
+            {
+                resultString += _title + " ";
+            }
+
+            if (_confidence != null)
+            {
+                resultString += string.Format("({0}%) ", _confidence * 100.0f);
+            }
+
+            return resultString.Trim();
+        }
+    }
+}
---
+++ customimageclassifier/imageclassifier/DescComparer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace imageclassifier
+{
+    internal partial class TensorFlowHelper
+    {
+        public class DescComparer<T> : IComparer<T>
+        {
+            public int Compare(T x, T y)
+            {
+                int result = Comparer<T>.Default.Compare(y, x);
+
+                if (result == 0)
+                    return 1;   // Handle equality as beeing greater
+                else
+                    return result;
+            }
+        }
+    }
+}
